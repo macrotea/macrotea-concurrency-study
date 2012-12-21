ac78586a4b261201bbfee482dca383bb930ad93c
liangqiye <macrotea@qq.com>
2012-12-21 14:47:52 +0800

    Initial commit

---
+++ src/main/java/com/mtea/macrotea_concurrency_study/App.java
@@ -0,0 +1,13 @@
+package com.mtea.macrotea_concurrency_study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
